e70c3e66483e77d35cf5c68ca93e60f33e0bdcac
Oxalys <s.yonte.montero@gmail.com>
2020-05-11 18:13:11 +0200

    integration du template

---
+++ resources/views/PageContact/pageContact.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('content')
+
+    @include('components.nav')
+    @include('pageContact.maps')
+    @include('pageContact.formulaire')
+    @include('components.footer')
+    
+@endsection
---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,48 @@
+<header>
+    <!-- Header Start -->
+   <div class="header-area header_area header-transparent">
+        <div class="main-header">
+           <div class="header-bottom  header-sticky">
+                <div class="container-fluid">
+                    <div class="row align-items-center">
+                        <!-- Logo -->
+                        <div class="col-xl-2 col-lg-1 col-md-1">
+                            <div class="logo">
+                              <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+                            </div>
+                        </div>
+                        <div class="col-xl-8 col-lg-8 col-md-8">
+                            <!-- Main-menu -->
+                            <div class="main-menu f-right d-none d-lg-block">
+                                <nav> 
+                                    <ul id="navigation">        
+                                        <li><a href="index.html">Home</a></li>
+                                        <li><a href="about.html">Offres</a></li>
+                                        <li><a href="services.html">Talents</a></li>
+                                        <li><a href="work.html">Entreprises</a></li>
+                                        <li><a href="blog.html">Login</a>
+                                            <ul class="submenu">
+                                                <li><a href="blog.html">Inscription</a></li>
+                                                <li><a href="single-blog.html">Profile</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>             
+                        <div class="col-xl-2 col-lg-3 col-md-3">
+                            <div class="header-right-btn f-right d-none d-lg-block">
+                                <a href="#" class="btn header-btn">Contact Now</a>
+                            </div>
+                        </div>
+                        <!-- Mobile Menu -->
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+           </div>
+        </div>
+   </div>
+    <!-- Header End -->
+</header>
