a2fb5d76f137153343b9142022ccba6b1caab8f3
Oxalys <s.yonte.montero@gmail.com>
2020-05-12 18:04:19 +0200

    color change & contact blade fixed

--- resources/views/PageContact/formulaire.blade.php
+++ resources/views/PageContact/formulaire.blade.php
@@ -1,5 +1,5 @@
-<section class="contact-section">
-<div class="row container">
+<section class="contact-section container">
+<div class="row">
     <div class="col-12">
         <h2 class="contact-title">Get in Touch</h2>
     </div>
