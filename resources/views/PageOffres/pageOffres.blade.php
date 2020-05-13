edadc86acfd791334ddb9d6741ca6ee3aae869d3
Oxalys <s.yonte.montero@gmail.com>
2020-05-13 18:05:06 +0200

    debut blade offre

--- resources/views/PageOffres/pageOffres.blade.php
+++ resources/views/PageOffres/pageOffres.blade.php
@@ -3,6 +3,20 @@
 @section('content')
 
     @include('components.nav')
+    <!-- Hero Area Start -->
+    <div class="hero-area hero-height2 d-flex align-items-center" data-background="{{asset('img/hero/h2_hero.jpg')}}">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="hero-cap text-center pt-50">
+                        <h2>Offres</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Hero Area End -->
+    
     @include('components.footer')
     
 @endsection
