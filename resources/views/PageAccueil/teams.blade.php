dbbafc18096552694cd0529bf315e57c970fb4d3
Oxalys <s.yonte.montero@gmail.com>
2020-05-13 16:37:50 +0200

    teams sans img

--- resources/views/PageAccueil/teams.blade.php
+++ resources/views/PageAccueil/teams.blade.php
@@ -3,37 +3,31 @@
         <div class="our-services all-starups-area pt-50 py-150">
             <div class="starups2">
             <div class="container no-gutter">
-                <div class="row">
+                <div class="row starups-details2">
                     <div class="col-lg-4 col-md-6 col-sm-6">
-                        <div class="single-services text-center mb-30">
-                            <div class="services-ion">
-                                <span class="flaticon-experience"></span>
-                            </div>
-                            <div class="services-cap">
-                                <h5>Strategic Planning</h5>
+                        <div class="single-services text-center mb-10">
+                            {{-- <div class="startup-img"><img src="img/gallery/startup_bg.jpg" alt=""></div> --}}
+                            <div class="services-cap2">
+                                <h5>Human Resource Talent</h5>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 col-sm-6">
-                        <div class="single-services text-center mb-30">
-                            <div class="services-ion">
-                                {{-- <div class="starups-img"></div> --}}
-                            </div>
-                            <div class="services-cap">
-                                <h5>Trades & stocks</h5>
+                        <div class="single-services text-center mb-10">
+                            {{-- <div class="starups-img"></div> --}}
+                            <div class="services-cap2">
+                                <h5>Webmaster</h5>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 col-sm-6">
-                        <div class="single-services text-center mb-30">
-                            <div class="services-ion">
-                                <span class="flaticon-good"></span>
-                            </div>
-                            <div class="services-cap">
-                                <h5>Audit & Assurance</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                        <div class="single-services text-center">
+                            {{-- <div class="starups-img3"></div> --}}
+                            <div class="services-cap2">
+                                <h5>Human Resource Entreprise</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</p>
                             </div>
                         </div>
                     </div>
@@ -42,53 +36,3 @@
         </div>
         </div>
         <!-- Our Services End -->
-
-<!--All startups Start -->
-<div class="all-starups-area fix">
-    <!-- left Contents -->
-    <div class="starups">
-        <div class="starups-details">
-            <span>Features That You Will Get</span>
-            <h3>All startups need to make<br>their business work</h3>
-            <!-- Details -->
-            <div class="starups-list">
-                <ul>
-                    <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30px" height="30px">
-                        <path fill-rule="evenodd"  fill="rgb(4, 219, 236)" d="M15.000,0.000 C23.284,0.000 30.000,6.716 30.000,15.000 C30.000,23.284 23.284,30.000 15.000,30.000 C6.716,30.000 -0.000,23.284 -0.000,15.000 C-0.000,6.716 6.716,0.000 15.000,0.000 Z"/>
-                        <text kerning="auto" font-family="themify" fill="rgb(255, 255, 255)" font-size="14px" x="9px" y="22.0320000000002px">&#58956;</text>
-                        </svg>
-                    </li>
-                    <li>
-                        <p>Having a home based business is a wonderful asset to your life. The problem still stands,</p>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30px" height="30px">
-                        <path fill-rule="evenodd"  fill="rgb(4, 219, 236)" d="M15.000,0.000 C23.284,0.000 30.000,6.716 30.000,15.000 C30.000,23.284 23.284,30.000 15.000,30.000 C6.716,30.000 -0.000,23.284 -0.000,15.000 C-0.000,6.716 6.716,0.000 15.000,0.000 Z"/>
-                        <text kerning="auto" font-family="themify" fill="rgb(255, 255, 255)" font-size="14px" x="9px" y="22.0320000000002px">&#58956;</text>
-                        </svg>
-                    </li>
-                    <li>
-                        <p>Promotional Advertising Specialty You Ve Waited Long Enough</p>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30px" height="30px">
-                        <path fill-rule="evenodd"  fill="rgb(4, 219, 236)" d="M15.000,0.000 C23.284,0.000 30.000,6.716 30.000,15.000 C30.000,23.284 23.284,30.000 15.000,30.000 C6.716,30.000 -0.000,23.284 -0.000,15.000 C-0.000,6.716 6.716,0.000 15.000,0.000 Z"/>
-                        <text kerning="auto" font-family="themify" fill="rgb(255, 255, 255)" font-size="14px" x="9px" y="22.0320000000002px">&#58956;</text>
-                        </svg>
-                    </li>
-                    <li>
-                        <p>Direct Mail Advertising How I Made 47 325 In 30 Days  By Mailing 2 200 Letters</p>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <!--Right Contents  -->
-    <div class="starups-img"></div>
-</div>
-<!--All startups End -->
