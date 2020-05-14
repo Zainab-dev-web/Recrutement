2874ff819056c96642554a569f8495636aa5e534
Zainab-dev-web <zainabfahem96@gmail.com>
2020-05-14 18:36:14 +0200

    agenda parti-1:design

--- src/Date/Month.php
+++ src/Date/Month.php
@@ -3,38 +3,60 @@ namespace App\Date;
 
 class Month{
 
+    public $days = ['Lundi' , 'Mardi', 'Mercredi' , 'Jeudi' , 'Vendredi' , 'Samedi','Dimanche'];
     private $months = [ 'Janvier' , 'Février' , 'Mars' , 'Avril' , 'Mai' , 'Juin' , 'Juillet' , 'Aout' , 'Septembre' , 'October' , 'Novembre', 'Décembre' , 'Décembre'];
 
+    public $month;
+    public $year;
 
     public function __construct(?int $month = null, ?int $year = null){
-        if($month === null){
+        if($month === null || $month< 1 || $month > 12){
             $month = intval(date(format:'m'));
         }
         if($year === null){
             $year = intval(date(format:'Y'));
         }
-        $month= $month % 12;
+        
         $this ->month = $month;
         $this->year = $year;
     }
+
+public function getStartingDay() : \DateTime{
+    return new \DateTime(time:"{$this->year}-{$this->month}-01");
+}
+
     public function toString(): string{
         return $this->months[$this->month -1] . ' ' . $this->year;
 
     }
     public function getWeeks() : int{
-<<<<<<< HEAD
-        
-        $start =new \DateTime(time:"{$this->year}-{$this->month}-01");
-        $end = (clone $start)->modify(modify: '+1 month -1 day');
-        $weeks = intval($end->format(format: 'W')) -intval($start->format(format:'W'));
-=======
-        $start =new \DateTime(time:"{$this->year}-{$this->month}-01");
+        $start = $this->getStartingDay();
         $end = (clone $start)->modify(modify: '+1 month -1 day');
         $weeks = intval($end->format(format: 'W')) -intval($start->format(format:''W));
->>>>>>> master
         if($week < 0){
             $weeks =intval($end->format(format: 'W'));
         }
         return $weeks;
+    }
+    public function  withinMonth (\DateTime $date): bool {
+    return $this->getStartingDay->format(format: 'Y-m') === $date->format(format: 'Y-m');
+    }
+    public function nextMonth(): Month{
+        $month= $this->month +1;  
+        $year =$this->year;
+        if($month > 12){
+            $month =1;
+            $year +=1;
+        }
+        return  new Month($month , $year);
+    }
+    public function previousMonth(): Month{
+        $month= $this->month -1;  
+        $year =$this->year;
+        if($month < 12){
+            $month =1;
+            $year -=1;
+        }
+        return  new Month($month , $year);
     }
 }
