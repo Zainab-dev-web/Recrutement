16cf72a11ae74c8d4f8f8511930b49dd0e06280b
Oxalys <s.yonte.montero@gmail.com>
2020-05-15 13:23:39 +0200

    talent seeder create

--- resources/views/PageOffres/offres.blade.php
+++ resources/views/PageOffres/offres.blade.php
@@ -67,7 +67,7 @@
                     </aside>
 
                     <aside class="single_sidebar_widget post_category_widget">
-                        <h4 class="widget_title">Category</h4>
+                        <h4 class="widget_title">Domaines</h4>
                         <ul class="list cat-list">
                             <li>
                                 <a href="#" class="d-flex">
