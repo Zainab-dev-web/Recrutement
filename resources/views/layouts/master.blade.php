0e2671e752f57d5cf96ef8486a02d122fe41f61e
Oxalys <s.yonte.montero@gmail.com>
2020-05-27 16:24:03 +0200

    visu profil edit2

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,7 +23,7 @@
             <div class="preloader-inner position-relative">
                 <div class="preloader-circle"></div>
                 <div class="preloader-img pere-text">
-                    <img src="{{asset('img/logo/loder.jpg')}}" alt="">
+                    <img src="{{asset('img/logo/loder.png')}}" alt="">
                 </div>
             </div>
         </div>
